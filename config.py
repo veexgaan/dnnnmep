273cf7a8bacb186c722dfd43f2349d5a6d159ace
veexgaan <85389396+veexgaan@users.noreply.github.com>
2021-10-20 03:41:06 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+import  os
+
+BOT_TOKEN = os.environ.get("2040509042:AAHnWrVn3FfnVMQTSg6GruwjgtcQmaVRzB4")
+APP_ID = int(os.environ.get("8326448"))
+API_HASH = os.environ.get("ecef7924a3340502450f8d33974743c4")
+
+youtube_next_fetch = 0  # time in minute
+
+
+EDIT_TIME = 5
